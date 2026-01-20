f7e2f6a62368b29cf4efb061387fd91564174d5b
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-20 14:15:34 +0800

    react

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom  '
+import Layout from './components/Layout/Layout'
+import Main from './page/Main'
+import About from './page/About'
+import Contact from './page/Contact'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>W collaboration</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Look at <code>src/App.jsx</code> and start coding if you can
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click the logos to learn or whatever
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<Main />} />
+          <Route path='about' element={<About />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='dashboard' element={<h1>Dashboard Page sidebar</h1>}>
+            <Route path='settings' element={<h1>Dashboard Settings Page</h1>} />
+            <Route path='profile' element={<h1>Dashboard Profile Page</h1>} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- playground/src/components/Layout/Header.jsx
+++ playground/src/components/Layout/Header.jsx
@@ -21,4 +21,4 @@ export default function Header() {
 
         </div>
     )
-}
+}a
