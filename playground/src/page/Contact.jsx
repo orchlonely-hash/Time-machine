32eb5aa8802c660706bdc5edfdcf575e11895c71
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-21 15:51:07 +0800

    another day file management

--- playground/src/page/Contact.jsx
+++ playground/src/page/Contact.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export default function Contact() {
     return (
-        <div className="w-full h-screen flex flex-col items-center justify-center p-4">
+        <div className="w-full h-screen flex flex-col items-center justify-center p-4 position-relative">
             <h2>Contact Us</h2>
             <p>If you have any questions, feel free to reach out!</p>
             <ul>
