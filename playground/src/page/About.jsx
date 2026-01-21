e084e4b6dafb055dde1b4bff9aa3612ecbef306e
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-21 16:51:04 +0800

    new stuff

--- playground/src/page/About.jsx
+++ playground/src/page/About.jsx
@@ -3,7 +3,7 @@ import TaskList from "../components/TaskList";
 
 export default function About() {
     return (
-        <div className="">
+        <div className="h-100vh p-4 w-100 relative -top-20 justify-cetner items-center flex flex-col">
             <h2>About Us</h2>
             <p>Welcome to our application! We are dedicated to providing the best experience for our users.</p>
             <p>Our team is passionate about building innovative solutions that make a difference.</p>
