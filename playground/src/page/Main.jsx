0793ea562029a0dba58f24cf9cb962069c7200f6
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-21 15:32:18 +0800

    another day

--- playground/src/page/Main.jsx
+++ playground/src/page/Main.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import Counter from "../components/Counter";
 
 
 export default function Main() {
+    const [count, setCount] = React.useState(0);
     return (
         <div className="w-full h-screen flex items-center flex-col">
-            <Counter />
+            <Counter count={count} setCount={setCount}/>
         </div>
     )
 }
