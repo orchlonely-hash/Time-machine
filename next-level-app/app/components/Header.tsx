8a132a75459018c97fdb9ecd5d0d872e19cbb8ef
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-01-29 17:00:00 +0800

    add .

---
+++ next-level-app/app/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="w-full h-40 bg-gray-800 text-white flex items-center justify-center">
+            <div className="w-full h-16 bg-gray-800 text-white flex items-center justify-center">
+                <h1 className="text-xl font-bold">Next Level App Fr</h1>
+            </div>
+        </header>
+    )
+}
