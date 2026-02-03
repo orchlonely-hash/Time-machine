0851001d4dd8fb59cef86291b322f9951cc71aee
Dweeb <orchlonely@gmail.com>
2026-02-03 19:47:27 +0800

    i discovered an unknown file

--- next-level-app/app/components/Header.tsx
+++ next-level-app/app/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className="w-full h-15 bg-gray-800 text-white flex items-center justify-center">
+        <header className="w-full h-15 bg-[var(--primary-color)] text-white flex items-center justify-center">
 
-            <div className="w-full h-full bg-gray-800 text-white flex items-center justify-center">
+            <div className="w-full h-full text-white flex items-center justify-center">
                 <h1 className="text-xl font-bold">How you doing</h1>
             </div>
 
--- next-level-app/app/page.tsx
+++ next-level-app/app/page.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full flex-col  bg-zinc-50 font-sans bg-[var(--background)] text-white justify-center p-2">
+    <main className="flex min-h-screen w-full flex-col bg-[var(--background-color)] font-sans text-white p-2">
       <div className="w-full h-64">
+
       </div>
 
     </main>
