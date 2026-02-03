b24ad59718d23624cde3e8441ffe048f8a8c3346
PADA-1-5-04 <PADA-1-5-04@Erxess-iMac.local>
2026-02-03 17:02:37 +0800

    cat

--- next-level-app/app/components/Header.tsx
+++ next-level-app/app/components/Header.tsx
@@ -1,12 +1,11 @@
 export default function Header() {
     return (
-        <header className="w-full h-15 bg-gray-800 text-white flex items-center justify-center">
+        <header className="w-full h-15 bg-gradient-to-b from-[var(--background)] to-[var(--secondary-color)] text-white flex items-center justify-center">
 
-            <div className="w-full h-full bg-gray-800 text-white flex items-center justify-center">
-                <h1 className="text-xl font-bold">How you doing</h1>
+            <div className="w-full h-full text-white flex items-center justify-center">
+                <h1 className="text-xl font-bold">So you wanna know about me</h1>
             </div>
 
-            
 
         </header>
     )
